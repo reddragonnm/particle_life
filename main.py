0f183102b668053ec6e41d6602c18b9ee6b37f39
reddragonnm <neeleshojmishra@gmail.com>
2024-10-03 00:38:25 +0530

    Add basic quadtree

--- main.py
+++ main.py
@@ -8,8 +8,132 @@ win_size = 800
 
 pg.init()
 screen = pg.display.set_mode((win_size, win_size))
+clock = pg.time.Clock()
 
 show_debug = False
+
+
+class Boundary:
+    def __init__(self, center, half_dim):
+        self.center = center
+        self.half_dim = half_dim
+
+    def contains_particle(self, particle):
+        return (
+            self.center[0] - self.half_dim
+            < particle.pos[0]
+            < self.center[0] + self.half_dim
+            and self.center[1] - self.half_dim
+            < particle.pos[1]
+            < self.center[1] + self.half_dim
+        )
+
+    def intersects(self, other):
+        self_min = self.center - self.half_dim
+        self_max = self.center + self.half_dim
+
+        other_min = other.center - other.half_dim
+        other_max = other.center + other.half_dim
+
+        return np.all(self_min < other_max) and np.all(self_max > other_min)
+
+
+class QuadTree:
+    def __init__(self, boundary):
+        self.capacity = 4
+        self.particles = []
+
+        self.nw = None
+        self.ne = None
+        self.sw = None
+        self.se = None
+
+        self.boundary = boundary
+
+    def insert(self, particle):
+        if not self.boundary.contains_particle(particle):
+            return
+
+        if len(self.particles) < self.capacity and self.nw is None:
+            self.particles.append(particle)
+            return
+
+        if self.nw is None:
+            self.subdivide()
+
+        self.nw.insert(particle)
+        self.ne.insert(particle)
+        self.sw.insert(particle)
+        self.se.insert(particle)
+
+    def subdivide(self):
+        center = self.boundary.center
+        half_dim = self.boundary.half_dim
+
+        self.nw = QuadTree(Boundary(center - half_dim / 2, half_dim / 2))
+        self.se = QuadTree(Boundary(center + half_dim / 2, half_dim / 2))
+
+        self.ne = QuadTree(
+            Boundary(center + [half_dim / 2, -half_dim / 2], half_dim / 2)
+        )
+        self.sw = QuadTree(
+            Boundary(center + [-half_dim / 2, half_dim / 2], half_dim / 2)
+        )
+
+        for p in self.particles:
+            self.nw.insert(p)
+            self.ne.insert(p)
+            self.sw.insert(p)
+            self.se.insert(p)
+        self.particles.clear()
+
+    def query(self, boundary):
+        particles_in_range = []
+
+        if not self.boundary.intersects(boundary):
+            return particles_in_range
+
+        for particle in self.particles:
+            if boundary.contains_particle(particle):
+                particles_in_range.append(particle)
+
+        if self.nw is None:
+            return particles_in_range
+
+        particles_in_range.extend(self.nw.query(boundary))
+        particles_in_range.extend(self.ne.query(boundary))
+        particles_in_range.extend(self.sw.query(boundary))
+        particles_in_range.extend(self.se.query(boundary))
+
+        return particles_in_range
+
+    def draw(self, screen):
+        pg.draw.rect(
+            screen,
+            (0, 100, 125),
+            (
+                *(self.boundary.center - self.boundary.half_dim),
+                self.boundary.half_dim * 2,
+                self.boundary.half_dim * 2,
+            ),
+            2,
+        )
+
+        if self.nw is not None:
+            self.nw.draw(screen)
+            self.ne.draw(screen)
+            self.sw.draw(screen)
+            self.se.draw(screen)
+
+    def clear(self):
+        self.nw = None
+        self.ne = None
+        self.sw = None
+        self.se = None
+        self.particles = []
+
+
+quad = QuadTree(Boundary(np.array([win_size / 2, win_size / 2]), win_size / 2))
 
 
 class Particle:
@@ -22,9 +146,6 @@ class Particle:
         self.r_max = self.size * 50
 
     def draw(self):
-        if show_debug:
-            pg.draw.circle(screen, (0, 0, 0, 0), self.pos, self.r_max, 2)
-
         pg.draw.circle(screen, colors[self.col], self.pos, self.size)
 
     def update_pos(self, dt):
@@ -65,10 +186,6 @@ class Particle:
                 (attraction_factor / (self.r_max - mp)) * (dist - mp)
             )
 
-        if show_debug and dist < mp * 2:
-            c = 255 * abs(attraction_factor)
-            pg.draw.line(screen, (c, c, c), self.pos, other.pos, 1)
-
         return direction * attraction * 10
 
     def update_vel(self, particles, dt):
@@ -83,9 +200,10 @@ class Particle:
 
 
 running = True
-dt = 0.2
+dt = 0.1
 
 particles = []
+i = 0
 
 
 while running:
@@ -99,33 +217,46 @@ while running:
             pos = np.array(pg.mouse.get_pos(), np.float64)
 
             r = 50
-            col = np.random.randint(0, len(colors))
+            col = i % 3
+            i += 1
 
             for r in range(50, 0, -20):
                 for angle in range(0, 360, 20):
-                    particles.append(
-                        Particle(
-                            pos
-                            + r
-                            * np.array(
-                                [
-                                    np.cos(angle * np.pi / 180),
-                                    np.sin(angle * np.pi / 180),
-                                ]
-                            ),
-                            np.array([0.0, 0.0]),
-                            col,
-                        )
+                    p = Particle(
+                        pos
+                        + r
+                        * np.array(
+                            [
+                                np.cos(angle * np.pi / 180),
+                                np.sin(angle * np.pi / 180),
+                            ]
+                        ),
+                        np.array([0.0, 0.0]),
+                        col,
                     )
+
+                    particles.append(p)
+
+                    quad.insert(p)
 
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_SPACE:
                 show_debug = not show_debug
 
+    quad.clear()
+    for p in particles:
+        quad.insert(p)
+
+    quad.draw(screen)
+
     for particle in particles:
         particle.update_pos(dt)
         particle.update_vel(particles, dt)
         particle.draw()
+
+    if show_debug:
+        clock.tick()
+        pg.display.set_caption(f"FPS: {int(clock.get_fps())}")
 
     pg.display.update()
 
